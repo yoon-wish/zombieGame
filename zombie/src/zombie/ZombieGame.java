38198c08f8376199dd930b51804bb18e78574f45
yoon-wish <sowon5685@gmail.com>
2024-03-26 16:59:30 +0900

    Modify : Game 클래스 생성에 따른 ZombieGame 클래스 수정

--- zombie/src/zombie/ZombieGame.java
+++ zombie/src/zombie/ZombieGame.java
@@ -1,15 +1,14 @@
 package zombie;
 
 public class ZombieGame {
-
-	public static void run() {
-		
-	}
 	
 	public static void main(String[] args) {
 		
 		ZombieGame.run();
 
 	}
-
+	
+	public static void run() {
+		Game.getInstance().run();
+	}
 }
