6dd6e3d9a02523f7c5e90a77abe2887312008d65
yoon-wish <sowon5685@gmail.com>
2024-03-26 14:10:21 +0900

    ADD : Dracula 클래스 작성

--- zombie/src/zombie/Dracula.java
+++ zombie/src/zombie/Dracula.java
@@ -1,6 +1,23 @@
 package zombie;
 
-public class Dracula {
-
-
+public class Dracula extends Unit {
+	int power;
+	String name = "Dracula";
+	
+	public Dracula(int pos, int hp, int max, boolean isEnemy) {
+		super(pos, hp, max, isEnemy);
+	}
+	
+	@Override
+	void attack(Unit hero) {
+		 power = (r.nextInt(this.getMax()) + 1) / 2;
+		 
+		 hero.setHp(hero.getHp() - power);
+		 if(hero.getHp() <= 0)
+			 hero.setHp(0);
+		 
+		 this.setHp(this.getHp() + power/2);	// 자신 회복
+		 
+		 System.out.printf("%s가 %d의 공격력으로 공격 : 현재 Hero hp: %d, 좀비 체력 회복 %d", name, power, hero.getHp(), this.getHp());
+	}
 }
