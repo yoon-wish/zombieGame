0933a46c6438825dcb946450a03f6c02cd6fdb05
yoon-wish <sowon5685@gmail.com>
2024-03-26 17:52:16 +0900

    ADD : Game 클래스 완성

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -11,7 +11,7 @@ public class Game {
 	Goast goast = new Goast("Goast", 5, 70, 6, true);
 	Zombie zombie = new Zombie("Zombie", 6, 80, 7, true);
 	Dracula dracula = new Dracula("Dracula", 7, 90, 8, true);
-	Boss boss = new Boss(9, 200, 15, 100, true);
+	Boss boss = new Boss("Boss", 9, 200, 15, 100, true);
 	
 	private Game() {
 		setGame();
@@ -45,19 +45,51 @@ public class Game {
 				meetMonster(zombie);
 			else if (heroPos == dracula.getPos())
 				meetMonster(dracula);
+			else if(heroPos == boss.getPos()) {
+				meetMonster(boss);
+			}
 		} else if(move == 2) {
 			isRun = false;
 		}
 	}
 	
 	private void meetMonster(Unit unit) {
-		System.out.printf("%s를 만났습니다. 공격모드로 바뀝니다.", unit.get);
+		System.out.printf("%s를 만났습니다. 공격모드로 바뀝니다.", unit.getName());
+		battleMonster(unit);
 	}
 	
-	private void move(int pos) {
-		this.pos = pos + 1;
+	private void battleMonster(Unit unit) {
+		System.out.print("공격하기(1), 포션마시기(2)");
+		int sel = sc.nextInt();
 		
+		if(sel == 1) {
+			unit.attack(hero);
+			hero.attack(unit);
+		} else if(sel == 2) {
+			hero.recovery();
+		}
+		
+		checkDead(unit);
 	}
+	
+	private void checkDead(Unit unit) {
+		if(hero.getHp() <= 0) {
+			System.out.println("Hero가 죽었습니다. 배틀에서 패배하였습니다.");
+			return;
+		}
+		
+		if(unit.getHp() <= 0) {
+			System.out.printf("%s를 이겼습니다.", unit.getName());
+			if(unit.getName().equals("Boss")) {
+				System.out.println(" 게임에서 승리했습니다. 종료합니다.");
+				return;
+			} else {
+				System.out.println(" 이동할 수 있습니다.");
+				return;
+			}
+		}
+	}
+	
 	public void run() {
 		while(true) {
 			move();
