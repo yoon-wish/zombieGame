6d723aba73d2860e8e4c14701f393eaf43da4f04
yoon-wish <sowon5685@gmail.com>
2024-03-26 14:18:26 +0900

    ADD : Boss 클래스 작성

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,5 +1,40 @@
 package zombie;
 
-public class Boss {
+import java.util.Random;
 
+public class Boss extends Zombie {
+	Random r = new Random();
+
+	private int shield;
+
+	public void setShield(int value) {
+		shield = value;
+	}
+
+	public int getShield() {
+		return shield;
+	}
+
+	public Boss(int pos, int hp, int max, int shield, boolean isEnemy) {
+		super(pos, hp, max, isEnemy);
+		this.shield = shield;
+	}
+	
+	public void attack(Unit hero) {
+		int a = r.nextInt(4) + 1;
+		int power = r.nextInt(getMax())+ 1;
+		
+		if(a == 1) {
+			System.out.println("보스의 필살기 발동 2배의 공격력");
+			hero.setHp(hero.getHp() - (power * 2));
+			if(hero.getHp() <= 0)
+				hero.setHp(0);
+		} else {
+			System.out.println("보스의 일반공격");
+			hero.setHp(hero.getHp() - power);
+			if(hero.getHp() <= 0)
+				hero.setHp(0);
+		}
+		System.out.printf("보스가 %d의 공격력으로 공격: 현재 Hero hp: %d", power, hero.getHp());
+	}
 }
