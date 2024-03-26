28cdf6b41e31ac618328dc27f82f098160b3def4
yoon-wish <sowon5685@gmail.com>
2024-03-26 17:04:29 +0900

    ADD : setGame에 setGame 메소드 추가

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -10,7 +10,15 @@ public class Game {
 		return instance;
 	}
 	
-	public void run() {
+	private void setGame() {
+		Hero hero = new Hero(1, 200, 20, 3, false);
+		Goast Goast = new Goast(5, 70, 6, true);
+		Zombie zombie = new Zombie(6, 80, 7, true);
+		Dracula Dracula = new Dracula(7, 90, 8, true);
+		Boss bos = new Boss(9, 200, 15, 100, true);
 		
+	}
+	public void run() {
+		setGame();
 	}
 }
