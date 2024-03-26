45264c78eb02c3a06cbad1c8e598d771b2dc4336
yoon-wish <sowon5685@gmail.com>
2024-03-26 14:32:33 +0900

    ADD : Hero 클래스 작성

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -1,5 +1,40 @@
 package zombie;
 
-public class Hero {
-
+class Hero extends Unit{
+	int power;
+	int count;
+	
+	public Hero(int pos, int hp, int max, int count, boolean isEnemy) {
+		super(pos, hp, max, isEnemy);
+		this.count = count;
+	}
+	
+	public void attack(Unit enemy) {
+		if(enemy instanceof Boss) {	// 적이 보스일 때
+			Boss boss = (Boss) enemy;
+			power = r.nextInt(getMax()) + 1;
+			
+			if(boss.getShield() > 0) { // 실드가 존재하면
+				int r = boss.getShield() - power;
+				if(r >=0) {	// 실드를 부시지 못했을 때
+					boss.setShield(boss.getShield() - power);
+				} else {	// 실드를 부셨을 때
+					boss.setShield(0);
+					boss.setHp(boss.getHp() - r);
+				}
+			} else 						// 실드가 존재하지 않으면
+				boss.setHp(boss.getHp() - power);
+			
+			if(boss.getHp() <= 0 )
+				boss.setHp(0);
+			
+			System.out.printf("히어로가 %d의 공격력으로 공격: 현재 Boss의 hp: %d 현재 Boss의 Shield: %d\n", power, boss.getHp(), boss.getShield());
+		} else {
+			power = r.nextInt(getMax()) + 1;
+			enemy.setHp(enemy.getHp() - power);
+			if(enemy.getHp()<=0)
+				enemy.setHp(0);
+		}
+		System.out.printf("히어로가 %d의 공격력으로 공격: 현재 Enemy hp: %d\n", power, enemy.getHp());
+	}
 }
