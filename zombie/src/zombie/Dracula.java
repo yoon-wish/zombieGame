8c5bb3259b891578c62502c96477dbf2a39611f8
yoon-wish <sowon5685@gmail.com>
2024-03-27 11:13:52 +0900

    ADD : recoverable 메소드 생성 및 오버라이드

--- zombie/src/zombie/Dracula.java
+++ zombie/src/zombie/Dracula.java
@@ -1,6 +1,6 @@
 package zombie;
 
-public class Dracula extends Unit {
+public class Dracula extends Unit implements RandomHit{
 	int power;
 
 	public Dracula(String name, int pos, int hp, int max, boolean isEnemy) {
@@ -23,4 +23,18 @@ public class Dracula extends Unit {
 		System.out.printf("[%s]가 %d의 공격력으로 공격\n현재 [Hero] hp: %d, [%s] 체력 회복 %d\n", this.getName(), power, hero.getHp(),
 				this.getName(), this.getHp());
 	}
+	
+	@Override
+	public void hitable(Unit hero) {
+		power = r.nextInt(3) + 1;	// 1 ~ 3 추가공격
+		
+		hero.setHp(hero.getHp() - power);
+		if(hero.getHp() <= 0)
+			hero.setHp(0);
+		
+		System.out.printf("[%s]가 %d의 공격력으로 추가공격!\n너무 빨라서 피할 수 없었다.\n", this.getName(), power);
+		System.out.printf("현재 [Hero] hp: %d\n", hero.getHp());
+	}
+	
+	
 }
---
+++ zombie/src/zombie/RandomHit.java
@@ -0,0 +1,5 @@
+package zombie;
+
+public interface RandomHit {
+	public void hitable(Unit unit);
+}
